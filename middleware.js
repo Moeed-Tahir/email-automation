9163b7ab9192bf83e6198e35a46a8916fdd3aaaf
abdoo <abdullah.k10204@gmail.com>
2025-04-28 13:13:01 +0500

    middleware update

--- middleware.js
+++ middleware.js
@@ -10,25 +10,25 @@ const protectedPaths = [
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  // if (pathname === '/') {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-  // const userEmail = request.cookies.get('userEmail')?.value;
-  // const userId = request.cookies.get('UserId')?.value;
-  // const token = request.cookies.get('Token')?.value;
+  const userEmail = request.cookies.get('userEmail')?.value;
+  const userId = request.cookies.get('UserId')?.value;
+  const token = request.cookies.get('Token')?.value;
 
-  // const isAuthenticated = userEmail && userId && token;
+  const isAuthenticated = userEmail && userId && token;
 
-  // if (pathname === '/login' && isAuthenticated) {
-  //   return NextResponse.redirect(new URL(`/${userId}/dashboard`, request.url));
-  // }
+  if (pathname === '/login' && isAuthenticated) {
+    return NextResponse.redirect(new URL(`/${userId}/dashboard`, request.url));
+  }
 
-  // const isProtected = protectedPaths.some((regex) => regex.test(pathname));
+  const isProtected = protectedPaths.some((regex) => regex.test(pathname));
 
-  // if (isProtected && !isAuthenticated) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (isProtected && !isAuthenticated) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
   return NextResponse.next();
 }
